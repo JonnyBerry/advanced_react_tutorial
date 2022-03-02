a8ea0803cda5b4af6c6dc41c29dfb8738290cbf1
jonathanberry <jonathanberry45@gmail.com>
2022-03-02 10:48:08 +0000

    compiled counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
+//import { useState } from 'core-js/library/es6/symbol';
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson] = useState({
+  name:'jonathan',
+  age:24,
+  message:'random message',
+});
+const [name,setName] = useState('peter')
+const [age,setAge] = useState('24')
+const [message,setmessage] = useState('random message')
+const changeMessage = () =>{
+  //setPerson ({ ...person, message:'hello world'});
+  setmessage('hello world');
+};
+
+  return (
+  <>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className='btn' onClick={changeMessage}>
+  change message
+  </button>
+
+  </>
+  );
 };
 
 export default UseStateObject;
