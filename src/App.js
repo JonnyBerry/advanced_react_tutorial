c123be07a60f30237f7939314320e6cc0a862b45
jonathanberry <jonathanberry45@gmail.com>
2022-03-04 01:00:18 +0000

    use effect basics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
 function App() {
   return(
   <div className="container">
     <Setup/>
     </div>
-);
+ );
 }
 
 export default App;
