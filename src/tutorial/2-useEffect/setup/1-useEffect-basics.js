36158259eb78060cce40202fb5744f9f03fbb73f
jonathanberry <jonathanberry45@gmail.com>
2022-03-07 00:43:46 +0000

    use effect clean up

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -10,9 +10,14 @@ const [value,setValue] = useState(0);
   if (value >= 1) {
   document.title =`New Messages(${value})`;
   }
-  });
+  }, [value]);
 
-  console.log('render component')
+  useEffect(()=>{
+    console.log('hello world');
+
+  }, []);
+
+  console.log('render component');
   return <>
   <h1>{value}</h1>
   <button className="btn" onClick={()=> setValue(value + 1)}>
