cf10b59fbf0143760d6c1feb404e9d6a7ba0648c
jonathanberry <jonathanberry45@gmail.com>
2022-03-12 19:32:15 +0000

    added bolean logic for return statements

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  const [loading,setLoading] = useState(false)
+  const [isLoading,setIsLoading] = useState(true);
+  const [isError,setIsError] = useState(false)
+  const [user,setUser] = useState('default user');
 
-  if (loading){
-    return <h2>loading...</h2>
+  if (isLoading) {
+    return <div>
+      <h1>Loading...</h1>
+    </div>;
   }
+  if (isError){
+return (
+ <div>
+      <h1>Error...</h1>
+    </div>
+    );
+  }
+
   return <h2>multiple returns</h2>;
 };
 
