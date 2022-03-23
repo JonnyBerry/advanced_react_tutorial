7f6a3e875d0d4b13c1dfcc13d4c26384ea8c19af
jonathanberry <jonathanberry45@gmail.com>
2022-03-23 00:51:00 +0000

    set modal content/function

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,16 +2,27 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state, action) => {
 
+}
+const defaultState = {
+  people:data,
+  isModalOpen: true,
+  modalContent:'hello world'
+}
 const Index = () => {
-  const [name,setName] = useState('')
-  const [people,setPeople] = useState(data);
-  const [showModal,setShowModal] = useState(false);
+  const [name,setName] = useState('');
+  const [state,dispatch] = useReducer(reducer,defaultState)
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (name){
+    }else{
+    }
   };
-  return <>
-  {showModal && <Modal/>}
+  return (
+  <>
+  {state.isModalOpen && <Modal modalContent={state.
+    modalContent}/>}
   <form onSubmit={handleSubmit} className='form'>
     <div>
       <input type="text" value={name} onChange={(e)=> setName(e.target.value)}
@@ -19,7 +30,15 @@ const Index = () => {
     </div>
     <button type="submit">add</button>
   </form>
-  </>;
+  {state.people.map((person) => {
+    return (
+    <div key={person.id}>
+      <h4>{person.name}</h4>
+    </div>
+  );
+  })}
+  </>
+  );
 };
 
 export default Index;
