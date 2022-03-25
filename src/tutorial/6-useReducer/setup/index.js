3dd8b452ed3ed17b4d6bfcb4419341fcfe263910
jonathanberry <jonathanberry45@gmail.com>
2022-03-25 19:14:11 +0000

    added modal timout

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,7 +3,6 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 const reducer = (state, action) => {
-  console.log(state);
 if (action.type === 'ADD_ITEM') {
   const newPeople = [...state.people,action.payload];
   return {
@@ -16,7 +15,9 @@ if (action.type === 'ADD_ITEM') {
     if(action.type === "NO_VALUE"){
     return{...state, isModalOpen:true, modalContent:'please enter value' };
     }
-
+  if (action.type === 'CLOSE_MODAL'){
+    return {...state,isModalOpen:false}
+  }
 throw new Error ('no matching action type');
 };
 const defaultState = {
@@ -38,9 +39,12 @@ const Index = () => {
       dispatch({type:'NO_VALUE'})
     }
   };
+  const closeModal = () => {
+    dispatch({type:'CLOSE_MODAL'})
+  }
   return (
   <>
-  {state.isModalOpen && <Modal modalContent={state.modalContent}/>}
+  {state.isModalOpen && <Modal closeModal={closeModal}modalContent={state.modalContent}/>}
   <form onSubmit={handleSubmit} className='form'>
     <div>
       <input type="text"
