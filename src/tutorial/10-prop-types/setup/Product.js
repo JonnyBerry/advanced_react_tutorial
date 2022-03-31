8d62e061043d2d1c2e2178213901a291f061235f
jonathanberry <jonathanberry45@gmail.com>
2022-03-31 23:50:06 +0100

    advanced react/react router

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import { product } from 'prelude-ls';
+import defaultImage from '../../../assets/default-image.jpeg';
 const Product = ({image,name,price}) => {
-  console.log(image,name,price)
+  const url = image && image.url;
   return (
 <article className='product'>
   <h4>single product</h4>
-   { /*<img src={image.url} alt={name}/>
+    <img src={url || defaultImage } alt={name || 'default name'}/>
     <h4>{name}</h4>
-    <p>${price}</p> */}
+    <p>${price || 3.99}</p>
   </article>
   );
 };
@@ -16,7 +17,10 @@ Product.propTypes = {
   image:PropTypes.object.isRequired,
   name:PropTypes.string.isRequired,
   price:PropTypes.number.isRequired
-
 }
-
+//Product.defaultProps = {
+  //name:'default name',
+  //price:3.99,
+ // image:defaultImage
+//}
 export default Product;
