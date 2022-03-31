27b38b4e3904a62218e0df69381d7ff90dae4331
jonathanberry <jonathanberry45@gmail.com>
2022-03-31 12:52:55 +0100

    set up product type and default props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
-const Product = () => {
-  return <article className='product'>single product</article>;
+import PropTypes from 'prop-types'
+import { product } from 'prelude-ls';
+const Product = ({image,name,price}) => {
+  console.log(image,name,price)
+  return (
+<article className='product'>
+  <h4>single product</h4>
+   { /*<img src={image.url} alt={name}/>
+    <h4>{name}</h4>
+    <p>${price}</p> */}
+  </article>
+  );
 };
+Product.propTypes = {
+  image:PropTypes.object.isRequired,
+  name:PropTypes.string.isRequired,
+  price:PropTypes.number.isRequired
+
+}
 
 export default Product;
